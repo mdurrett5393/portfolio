47c4b824dae05e2d1b70226b86d5e5dc763997c9
mdurrett5393 <mdurrett5393@gmail.com>
2026-07-04 18:05:35 -0400

    Quartz sync: Jul 4, 2026, 6:05 PM

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -1,7 +1,14 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
-  return <div id="quartz-body">{children}</div>
+  return (
+    <div id="quartz-body">
+      {children}
+      <footer style={{ padding: "1rem 0", fontSize: "0.85rem", color: "var(--gray)" }}>
+        © 2026 Mike Durrett. All rights reserved.
+      </footer>
+    </div>
+  )
 }
 
 export default (() => Body) satisfies QuartzComponentConstructor
